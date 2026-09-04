4e2cc3b7ab9ef49b6f08c57ec80ac4d48c8948d8
OoSantiagoSoO <114419928+XxSantiagO1xX@users.noreply.github.com>
2026-09-04 14:14:47 -0600

    Usa configuración central de API en menú móvil

--- nibiru-mobile/screens/ProductsScreen.js
+++ nibiru-mobile/screens/ProductsScreen.js
@@ -17,8 +17,7 @@ import { Ionicons } from "@expo/vector-icons";
 import ProductCard from "../components/ProductCard";
 import { AppContext } from "../context/AppContext";
 import colors from "../theme/colors";
-
-const API = "http://192.168.1.86:3000";
+import API from "../config/api";
 
 export default function ProductsScreen() {
   const [products, setProducts] = useState([]);
